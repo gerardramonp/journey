542458ca92b1ba356c41478434d07591c8796ed5
Gerard Ramon Monte <g.ramon@glofox.com>
2023-02-15 20:15:38 -0500

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Layout from './components/Layout';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes/routes';
 import useRouteProtection from './hooks/useRouteProtection';
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 function App() {
   useRouteProtection();
